fc668f09bf2ca44312bb09a47eedb4cd18ed3b49
Nitish <nitishchaudhary0609@gmail.com>
2025-08-25 01:20:32 +0530

    JWT based authentication

--- src/main/java/com/learning/SpringBootSelf/config/SecurityConfig.java
+++ src/main/java/com/learning/SpringBootSelf/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.learning.SpringBootSelf.config;
 
+import com.learning.SpringBootSelf.Utils.JwtFilter;
 import com.learning.SpringBootSelf.services.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +24,9 @@ public class SecurityConfig{
     @Autowired
     private UserDetailServiceImpl userDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests( req -> req
@@ -27,9 +34,10 @@ public class SecurityConfig{
                         .requestMatchers("/journal/**", "/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 //        return http.authorizeHttpRequests( req -> req
 //                        .requestMatchers("/public/**").permitAll()
@@ -49,6 +57,11 @@ public class SecurityConfig{
     @Autowired
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 
 }
--- src/main/java/com/learning/SpringBootSelf/controllers/PublicController.java
+++ src/main/java/com/learning/SpringBootSelf/controllers/PublicController.java
@@ -1,11 +1,16 @@
 package com.learning.SpringBootSelf.controllers;
 
 
+import com.learning.SpringBootSelf.Utils.JwtUtil;
 import com.learning.SpringBootSelf.entaties.User;
+import com.learning.SpringBootSelf.services.UserDetailServiceImpl;
 import com.learning.SpringBootSelf.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +23,42 @@ public class PublicController {
     @Autowired
     UserService service;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    @PostMapping("/add")
-    public ResponseEntity<?> addOne(@RequestBody User user){
-        try{
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody User user) {
+        try {
             service.saveEntry(user);
             return new ResponseEntity<>("Record saved", HttpStatus.ACCEPTED);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>("Problem during record insertion", HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> logIn(@RequestBody User user) {
+
+        try {
+            authenticationManager.authenticate((new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword())));
+
+            UserDetails userDetails = userDetailService.loadUserByUsername(user.getUserName());
+
+            String token= jwtUtil.generateToken(userDetails.getUsername());
+
+            return new ResponseEntity<>(token, HttpStatus.ACCEPTED);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>("Your Password or username is incorrect", HttpStatus.UNAUTHORIZED);
+        }
+
+    }
+
 }
